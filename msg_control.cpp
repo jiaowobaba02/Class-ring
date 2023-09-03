4c6e17b5866302492ec263336db3f5d2a9c84da9
yue-weikai <69029820+jiaowobaba02@users.noreply.github.com>
2023-09-03 10:15:42 +0800

    Update msg_control.cpp

--- msg_control.cpp
+++ msg_control.cpp
@@ -1,15 +1,48 @@
-#include<iostream>
-#include<windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
-using namespace std;
-void msg(char *title,char *info){
-    MessageBox(GetForegroundWindow(),title,info,4);
-}
-int main(int argc,char *argv[]){
-    if(argc==3){
-    cout<<"showed"<<" title"<<argv[1]<<" info: "<<argv[2]<<endl;
-    msg(argv[1],argv[2]);
-   }else{
-    cout<<argv[0]<<":Usage:title infomation";
+int main(int argc, char *argv[])
+{
+    // 检查参数个数
+    if (argc != 4)
+    {
+        printf("Usage: ./msg_control.exe info title type\n");
+        return 1;
     }
+
+    // 获取参数
+    char *info = argv[1]; // 消息框内容
+    char *title = argv[2]; // 消息框标题
+    char *type = argv[3]; // 消息框类型
+
+    // 根据type的值选择消息框类型
+    UINT msg_type;
+    if (strcmp(type, "w") == 0 || strcmp(type, "W") == 0)
+    {
+        msg_type = MB_ICONWARNING;
+    }
+    else if (strcmp(type, "e") == 0 || strcmp(type, "E") == 0)
+    {
+        msg_type = MB_ICONERROR;
+    }
+    else if (strcmp(type, "i") == 0 || strcmp(type, "I") == 0)
+    {
+        msg_type = MB_ICONINFORMATION;
+    }
+    else
+    {
+        printf("Invalid type: %s\n", type);
+        return 2;
+    }
+
+    // 创建消息框
+    int result = MessageBox(NULL, info, title, msg_type);
+    if (result == 0)
+    {
+        printf("Failed to create message box.\n");
+        return 3;
+    }
+
+    return 0;
 }
