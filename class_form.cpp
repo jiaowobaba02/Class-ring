3b783661edff1663a381e98929a98b39c90729b1
yue-weikai <69029820+jiaowobaba02@users.noreply.github.com>
2023-10-08 21:39:33 +0800

    Update class_form.cpp

--- class_form.cpp
+++ class_form.cpp
@@ -73,12 +73,18 @@ int main(int argc, char* argv[]){ // 加上int返回值类型，否则编译错�
 }
    int class_now = atoi(argv[2]); // 使用atoi函数来把字符串转换成整数，并检查是否合法
    class_ring_input=class_ring_input +" ";
-   class_ring_input=class_ring_input +  data[class_now][week+1]; // 直接拼接data[class_now][week+1]，并注意下标从0开始
+    if(week==0){
+      class_ring_input=class_ring_input +  data[class_now][8];
+   }else
+    {
+      class_ring_input=class_ring_input +  data[class_now][week+1];
+   } 
+    // 直接拼接data[class_now][week+1]，并注意下标从0开始
    class_ring_input=class_ring_input + start_or_not;
    class_ring_input=class_ring_input + " i";
    cout<<class_ring_input<<endl;
    
-   system(class_ring_input.c_str()); // 使用exec函数或者其他更高级的方法来执行命令，而不是system函数，用c_str()方法来把string对象转换成const char*类型的指针
+   system(class_ring_input.c_str()); 
    
 }
 return 0; // 返回0，表示程序正常结束
